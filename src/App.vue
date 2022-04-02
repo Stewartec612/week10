0de187d4fc57a0abdeef737b348c514e7a63b821
Stewart Christie <lt3778xi@go.minnstate.edu>
2022-04-02 10:06:22 -0500

    Ran into some errors i couldnt figure out

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,61 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <h1>Would You Rather...</h1>
+
+    <would-you-rather
+
+      v-for="question in questions"
+      v-bind:question="questions.question"
+      v-bind:answer1="questions.answer1"
+      v-bind:answer2="questions.answer2"
+      v-on:answer-changed="answerChanged"
+    ></would-you-rather>
+
+    <p>{{ userSelectionMessage }}</p>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WouldYouRather from './components/WouldYouRather.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    WouldYouRather
+  },
+  data(){
+    return {
+      questions:[
+        {
+          id: 0,
+          question: 'would you rather be incredibly funny or incredibly smart?',
+          answer1: 'incredibly funny',
+          answer2: 'incredibly smart',
+        },
+        {
+          id: 1,
+          question: 'Would you rather have super speed or be able to go invisible?',
+          answer1: 'super speed',
+          answer2: 'go invisible'
+        },
+        {
+          id: 3,
+          question: 'Would you rather brush your teeth with soap or drink sour milk?',
+          answer1: 'brush teeth with soap',
+          answer2: 'drink sour milk'
+        }
+
+      ],
+      userSelectionMessage: ''
+    }
+  },
+  methods: {
+    
+    answerChanged(choice) {
+      this.userSelectionMessage = `Thanks! you chose ${ choice }`
+    }
   }
 }
 </script>
---
+++ src/components/WouldYouRather.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="wyr">
+
+<!--    <h2>Please make your choice</h2>-->
+<!--    incredibly funny or incredibly smart?-->
+    <h3>{{ question }}</h3>
+
+    <input type="radio" v-model="choice" v-bind:value="answer1" v-on:change="choice">
+    <label>{{ answer1 }}</label>
+
+    <input type="radio" v-model="choice" v-bind:value="answer2" v-on:change="choice">
+    <label>{{ answer2 }}</label>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WouldYouRather',
+  props: {
+    question: String,
+    answer1: String,
+    answer2: String,
+
+  },
+  data() {
+    return{
+      choice: ''
+    }
+  },
+  methods: {
+    choiceMade() {
+      this.$emit('answer-changed', this.choice)
+    },
+    questions(){}
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+</style>
